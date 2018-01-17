61dc82627263a9f4cffc5c495c42ffd446b74795
Graydon Armstrong <seeds77222@hotmail.com>
2018-01-17 10:03:13 -0500

    Initial Commit
    Include Foundation files, and basic index page

---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+
+<html class="no-js" lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Graydon Armstrong</title>
+        <meta name="description" content="Graydon Armstrong's Portfolio of Work">
+        <meta name="author" content="Graydon Armstrong">
+
+        <link rel="stylesheet" href="vendors/foundation/css/foundation.css">    
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+
+    <body>
+
+        
+        <div class="text-center">
+            <img class="thumbnail " width="200px;" src="resources/images/me.jpg">
+            <h1>Graydon Armstrong</h1>
+        </div>
+        
+        <div class="text-center">
+            <p>Software Engineer</p>
+            <p>Photographer</p>            
+        </div>
+
+        <div class="text-center">
+            <h2>Contact</h2>
+            <p>Email: graydon.armstrong@live.ca</p>          
+        </div>
+        
+        <footer>
+            
+        </footer>        
+
+        <script src="vendors/foundation/js/jquery.js"></script>
+        <script src="vendors/foundation/js/what-input.js"></script>
+        <script src="vendors/foundation/js/foundation.js"></script>
+        <script src="vendors/foundation/js/app.js"></script>
+    </body>
+</html>
